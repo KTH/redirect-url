9e0a4db3e3a22ef2720707c89a25a9355077761f
Patric Jansson <paddy@kth.se>
2021-02-12 23:37:04 +0100

    Integration test fix

--- tests/integration-tests/basic.sh
+++ tests/integration-tests/basic.sh
@@ -3,11 +3,9 @@
 info() { printf "\033[1;31m\n   %s\033[0;33m$@\n\n";  }
 error() { printf "\033[1;31m • $@\033[0;0m\n"; }
 passed() { printf "\033[0;32m • $@\033[0;0m\n"; }
-#
-# Path to the Cisco vpn client.
-#
+
 if [ -z "$URL_PREFIX" ]; then
-    URL_PREFIX="http://web"
+    URL_PREFIX="http://web/some-path"
     sleep 5s
 fi
 
@@ -48,10 +46,50 @@ expectPathToContain() {
 
 }
 
+#
+# Curls a url and tests if the response contains a string.
+# If it fails sets FAILED to true.
+#
+# Usage: expectPathToContain "/_monitor" "active"
+#
+expectPathToContainHeader() {
+    
+    ENDPOINT="$1"
+    PATTERN="$2"
+    TEST_DESCRIPTION="$3"
+    
+    TEST_URL="$URL_PREFIX$ENDPOINT"
+
+    curl -I -k -S --max-time 3 $TEST_URL > .curl.log 2>&1
+    RESULT=$(cat .curl.log)
+    
+    if [[ "$RESULT" == *"$PATTERN"* ]]; then
+        if [ ! -z "$TEST_DESCRIPTION" ]; then
+            passed "$TEST_DESCRIPTION."
+        else 
+            passed "$TEST_URL contains $PATTERN"
+        fi
+ 
+    else
+        if [ ! -z "$TEST_DESCRIPTION" ]; then
+            error "$TEST_DESCRIPTION"
+        fi
+        info "'$TEST_URL' does not contain headers '$PATTERN'."
+
+        cat .curl.log
+        
+        FAILED="true"
+    fi
+
+}
+
 # ---------------- Tests ----------------
 
 expectPathToContain "/_monitor" "APPLICATION_STATUS: OK" "Default check APPLICATION_STATUS: OK"
 expectPathToContain "/_about" "Docker image" "The about page should show Docker images information"
+expectPathToContainHeader "/_about" "200 OK" "_about should return 200 Ok response"
+expectPathToContainHeader "/_monitor" "200 OK" "_monitor should return 200 Ok response"
+expectPathToContainHeader "/redirect/this/path" "301 Moved Permanently" "A redirect path should 307 Temporary"
 
 # Result
 if [[ "$FAILED" != *"true"* ]]; then
